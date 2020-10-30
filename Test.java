933ba92e084ad5d8e7d1321fdb8936e9da3c8b23
Roman Verekh <vrbox@i.ua>
2020-10-30 16:54:26 +0200

    Change last string

--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@ public class Test {
     public static void main(String[] args) {
         
     System.out.println("Hello word!");
-    System.out.println("New String!");
+    System.out.println("Change String!");
 
     }
 }
