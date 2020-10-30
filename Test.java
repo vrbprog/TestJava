88bcad90d20fd70278b17412a351b25ab0568ef5
Roman Verekh <vrbox@i.ua>
2020-10-30 16:47:43 +0200

    Add new string

--- Test.java
+++ Test.java
@@ -1,7 +1,8 @@
 public class Test {
     public static void main(String[] args) {
         
-		System.out.println("Hello word!");
+    System.out.println("Hello word!");
+    System.out.println("New String!");
 
     }
 }
